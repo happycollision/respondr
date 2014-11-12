2d92af7beb57bced2455112bb192478f8516f1bf
Don Denton <don@happycollision.com>
2014-11-12 02:14:46 -0600

    Successfully calls an image from Flickr

--- src/respondr.js
+++ src/respondr.js
@@ -11,22 +11,56 @@
   // Collection method.
   $.fn.respondr = function () {
     return this.each(function (i) {
+      var that = this;
       // Do something to each selected element.
-      $(this).html('respondr' + i);
+      $.respondr(2423496036).done( function(data){
+        $(that).html('<img src="'+data.sizes.size[6].source+'">');
+      });
     });
   };
 
   // Static method.
   $.respondr = function (options) {
-    // Override default options with passed-in options.
-    options = $.extend({}, $.respondr.options, options);
-    // Return the name of your plugin plus a punctuation character.
-    return 'respondr' + options.punctuation;
+    var method = 'flickr.photos.getSizes';
+    var format = 'json';
+
+    if (options === null || typeof options === "undefined") {
+      // TODO: Throw an error
+      return;
+    }
+
+    if (typeof options === "object") {
+      // Assume it's actually an options hash
+      // Override default options with passed-in options.
+      options = $.extend($.respondr.options, options);
+    
+    } else if (typeof options === "string" || typeof options === "number"){
+      // We make the call
+      var photoId = options;
+
+      return $.ajax({
+        url: 'https://api.flickr.com/services/rest/',
+        data: {
+          method: method,
+          format: format,
+          api_key: $.respondr.options.apiKey,
+          photo_id: photoId,
+          nojsoncallback: 1
+        }
+
+      });
+    
+    } else {
+      // TODO: Throw an error
+      return;
+    }
+
+
   };
 
   // Static method default options.
   $.respondr.options = {
-    punctuation: '.'
+    apiKey: ''
   };
 
   // Custom selector.
