5e7beea7c7c27c6209d49281bea9389465c9c725
Don Denton <don@happycollision.com>
2014-11-13 19:16:34 -0600

    Shortcode replacement works
    Shortcode is replaced with all images returned at the moment. Shortcode
    is also probably fairly brittle.

--- src/respondr.js
+++ src/respondr.js
@@ -10,12 +10,41 @@
 
   // Collection method.
   $.fn.respondr = function () {
-    return this.each(function (i) {
-      var that = this;
-      // Do something to each selected element.
-      $.respondr(2423496036).done( function(data){
-        $(that).html('<img src="'+data.sizes.size[6].source+'">');
+
+    var flickrData = {}; // object where flickr id is the index of the returned data
+    // ie { "1239847239": {flickr data promise}, "90384752": {flickr data promise} }
+
+    var createHtmlFromFlickrResponse = function(response){
+      var output = '';
+      $.each(response.sizes.size, function(i, size){
+        output += '<img src="'+ size.source +'">';
       });
+      return output;
+    };
+
+    var getFlickrDataAndWrapShortcodes = function(element){
+      var pattern = /\[ {0,1}respondr (.*?)\]/ig;
+      newHtml = $(element).html().replace(pattern, function(match, $1){
+        flickrData[$1] = $.respondr($1);
+        return '<span class="respondr-return" data-respondr-id="'+ $1 +'">' +
+          match + '</span>';
+      });
+      $(element).html(newHtml);
+    }
+
+    return this.each(function () {
+
+      getFlickrDataAndWrapShortcodes(this);
+
+      var $respondrSpans = $(".respondr-return"); // for speed later
+
+      $.each(flickrData, function(i,promise){
+        promise.done( function(response){
+          var html = createHtmlFromFlickrResponse(response);
+          $respondrSpans.filter("[data-respondr-id='" + i + "']").html(html);
+        });
+      });
+
     });
   };
 
