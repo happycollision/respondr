a88fab38312f6b9b74e493ff8c607b794ea6b55d
Don Denton <don@happycollision.com>
2015-02-05 17:44:09 -0600

    Adds gallery-making ability

--- src/respondr.js
+++ src/respondr.js
@@ -13,6 +13,7 @@
     // object where flickr id is the index of the returned data
     // ie { "1239847239": {flickr data promise}, "90384752": {flickr data promise} }
     var flickrData = {};
+    var galleryData = {};
 
     if ( typeof mediaQuerySizes !== 'string' || mediaQuerySizes.length < 1 ) {
       mediaQuerySizes = '100vw';
@@ -57,12 +58,28 @@
       return output;
     };
 
+    var determineShortcodeType = function(respondrInput){
+      var galleryMatchPattern = / *gallery (.*)/i;
+      var galleryMatchResult = respondrInput.match(galleryMatchPattern);
+      if (galleryMatchResult !== null) {
+        return ['gallery', galleryMatchResult[1]];
+      } else {
+        return ['image', respondrInput];
+      }
+    };
+
     var getFlickrDataAndWrapShortcodes = function(element){
       var pattern = /\[ {0,1}respondr (.*?)\]/ig;
       var imgWrapper = $(element).html().replace(pattern, function(match, $1){
-        flickrData[$1] = $.respondr($1);
-        return '<span class="respondr-return" data-respondr-id="'+ $1 +'">' +
-          'replacing image' + '</span>';
+        var respondrTypeInfo = determineShortcodeType($1);
+        if (respondrTypeInfo[0] === 'image') {
+          flickrData[respondrTypeInfo[1]] = $.respondr(respondrTypeInfo[1]);
+          return '<span class="respondr-return" data-respondr-id="'+ $1 +'">' +
+            'replacing image' + '</span>';
+        } else if (respondrTypeInfo[0] === 'gallery') {
+          galleryData[respondrTypeInfo[1]] = $.respondr.gallery(respondrTypeInfo[1]);
+          return '<div class="respondr-return-gallery" data-respondr-id="'+ respondrTypeInfo[1] +'"></div>';
+        }
       });
       $(element).html(imgWrapper);
     };
@@ -93,6 +110,18 @@
           }
 
           $.respondr.options.callback($thisElement);
+        });
+      });
+
+      $.each(galleryData, function(i,promise){
+        promise.done( function(response){
+          var $thisElement = $('.respondr-return-gallery').filter("[data-respondr-id='" + i + "']");
+          $.each(response.photoset.photo, function(i, photoObj){
+            $thisElement.append('[respondr '+ photoObj.id +']');
+          });
+
+          $thisElement.respondr(mediaQuerySizes, includeSquares);
+
         });
       });
 
@@ -141,6 +170,19 @@
     apiKey: '',
     usePicturefill: false,
     callback: function(){return;}
+  };
+
+  $.respondr.gallery = function(photoId){
+    return $.ajax({
+      url: 'https://api.flickr.com/services/rest/',
+      data: {
+        method: 'flickr.photosets.getPhotos',
+        format: 'json',
+        api_key: $.respondr.options.apiKey,
+        photoset_id: photoId,
+        nojsoncallback: 1
+      }
+    });
   };
 
 }(jQuery));
